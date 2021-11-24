26cb0513a1cdb08d5d54fa2d0c6d1a7903574f5e
RomanOmelianenko <r_omelyanenko@ukr.net>
2021-11-24 22:28:57 +0200

    add to cart1

--- js/cart.js
+++ js/cart.js
@@ -2,70 +2,61 @@ const cart = function () {
   const cartBtn = document.querySelector(".button-cart");
   const cart = document.getElementById("modal-cart");
   const closeModalBtn = cart.querySelector(".modal-close");
-
-  // урок 4
   const goodsContainer = document.querySelector('.long-goods-list');
-  // ---------
-
-  // 4.6 теперь в самом модальном окне отрисовуем строчки товара
   const cartTable = document.querySelector('.cart-table__goods');
-  // при открытии модального окна нужно отрисовать вёрстку на основании вот этого массива данных    localStorage.setItem('cart', JSON.stringify(cart))
+
+  const modalForm = document.querySelector('.modal-form');
+  modalForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // console.log('submit');
+    sendForm()
+  })
 
   cartBtn.addEventListener("click", () => {
-    // 4.6.1 рендер товара
-    console.log('рендер товара');
+    // console.log('рендер товара');
     const cartArray = localStorage.getItem('cart') 
       ? JSON.parse(localStorage.getItem('cart')) 
       : []
     renderCartGood(cartArray)
-    // ------------------
+
     cart.style.display = "flex";
   });
 
   closeModalBtn.addEventListener("click", function () {
     cart.style.display = "";
-    // cartBtn.style.display = 'none';
   });
 
-  // 4.1
+  cart.addEventListener('click', (event) => {
+    if (!event.target.closest('modal') && event.target.classList.contains('overlay')) {
+      cart.style.display = ''
+    }
+  })
+ 
   if (goodsContainer) {
     goodsContainer.addEventListener('click', (event) => {
-      // console.log(event.target);
-      // if (event.target.classList.contains('add-to-cart')) {
-      //   console.log('кнопка');
-      // }
+ 
       if (event.target.closest('.add-to-cart')) {
-        // console.log('кнопка');
         const buttonToCart = event.target.closest('.add-to-cart');
-        // console.log(buttonToCart);
-        const goodDataId = buttonToCart.dataset.id; // получаем атрибут data-id кнопки
+        const goodDataId = buttonToCart.dataset.id;
 
         addToCart(goodDataId);
       }
     })
   }
 
-  // 4.2
   const addToCart = (id) => {
-    // console.log(goodId);
-    // при каждом клике получаем весь массив товаров
+  
     const goods = JSON.parse(localStorage.getItem('goods'));
-    // console.log(goods);
-    // среди этого массива нужно найти кликнутую карточку
+  
     const clickedGood = goods.find(good => good.id === id)
-    // console.log(clickedGood);
+    
     const cart = localStorage.getItem('cart') 
       ? JSON.parse(localStorage.getItem('cart')) 
       : []
 
-    // console.log(cart);
-    // console.log(clickedGood);
-
-    // 4.3 добавляем товар в корзину или увеличиваем его кол-во, если такой товар есть уже вкорзине. Используем метод some(возвр true или false), а не метод find(возвр undefined)
-    // console.log(cart.some(good => good.id === clickedGood.id));
     if (cart.some(good => good.id === clickedGood.id)) {
-      console.log('Увеличить количество clickedGood');
-      // 4.5
+      // console.log('Увеличить количество clickedGood');
+      
       cart.map(good => {
         if (good.id === clickedGood.id) {
           good.count += 1
@@ -73,19 +64,18 @@ const cart = function () {
         return good
       })
     } else {
-      console.log('Добавить в корзину');
-      clickedGood.count = 1; // дописываем одно свойство count
+      // console.log('Добавить в корзину');
+      clickedGood.count = 1; 
       cart.push(clickedGood);
     }
 
-    // 4.4 сохраняем новую корзинку в localStorage
     localStorage.setItem('cart', JSON.stringify(cart))
   }
 
-  // 4.6.2
   const renderCartGood = (goods) => {
-    // console.log(goods);
-    // перебираем массив и формируем верстку
+  
+    cartTable.innerHTML = '';
+  
     goods.forEach(good => {
       const tr = document.createElement('tr');
       tr.innerHTML = `
@@ -94,11 +84,80 @@ const cart = function () {
         <td><button class="cart-btn-minus"">-</button></td>
         <td>${good.count}</td>
         <td><button class=" cart-btn-plus"">+</button></td>
-        <td>${+good.price * good.count}$</td>
+        <td>${+good.price * +good.count}$</td>
         <td><button class="cart-btn-delete"">x</button></td>
       `
-      // ${+good.price * good.count} - приводим результат к числу
       cartTable.append(tr);
+
+      //уменьшение, добавление и удаление товаров в корзине
+      tr.addEventListener('click', (event) => {     
+        if (event.target.classList.contains('cart-btn-minus')) {
+          // console.log('минус');
+          minusCartItem(good.id)
+        } else if (event.target.classList.contains('cart-btn-plus')) {
+          // console.log('плюс');
+          plusCartItem(good.id)
+        } else if (event.target.classList.contains('cart-btn-delete')) {
+          // console.log('удалён');
+          deleteCartItem(good.id)
+        }
+      })
+    })
+  }
+
+  const deleteCartItem = (id) => {
+    
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    const newCart = cart.filter(good => {
+      return good.id !== id
+    }) 
+    
+    localStorage.setItem('cart', JSON.stringify(newCart))
+    
+    renderCartGood(JSON.parse(localStorage.getItem('cart')))
+  }
+
+  const plusCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    const newCart = cart.map(good => {
+      if (good.id === id) {
+        good.count += 1
+      }
+      return good
+    })
+    localStorage.setItem('cart', JSON.stringify(newCart))
+    renderCartGood(JSON.parse(localStorage.getItem('cart')))
+  }
+
+  const minusCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    const newCart = cart.map(good => {
+      if (good.id === id) {
+        
+        if (good.count > 0) {
+          good.count -= 1
+        }
+      }
+      return good
+    })
+    localStorage.setItem('cart', JSON.stringify(newCart))
+    renderCartGood(JSON.parse(localStorage.getItem('cart')))
+  }
+
+  const sendForm = () => {
+    const cartArray = localStorage.getItem('cart') 
+      ? JSON.parse(localStorage.getItem('cart')) 
+      : []
+    // Обращаемся к тестовому API
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        cart: cartArray,
+        name: '',
+        phone: ''
+      })
+    }).then(() => {
+      cart.style.display = ''
     })
   }
 };
