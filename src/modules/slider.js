477bd9c537e626330946d9d6ce4cf973ca077638
RomanOmelianenko <r_omelyanenko@ukr.net>
2021-12-04 12:09:59 +0200

    swiper

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -3,11 +3,10 @@ import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay]);
 
 const slider = () => {
-  // экзкмпляр класса Swiper
-  const swiper = new Swiper('.swiper-container', //передаём класс из index.html (77 срока)
-  {      //передаём обьукт настроек из документации swiper       
-    loop: true,    // добавляем клик по кругу
-    autoplay: {    //добавляем автопрокрутку
+  const swiper = new Swiper('.swiper-container',
+  {          
+    loop: true,    
+    autoplay: {    
       delay: 2500,
       disableOnInteraction: false,
     },
